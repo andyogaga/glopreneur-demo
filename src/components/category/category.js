f910c4f978480aef2a8415a60c9398a4f1d7158f
shleeq <support@shleeq.com>
2019-06-06 18:49:15 +0100

    Updated the hover and menu size

--- src/components/category/category.js
+++ src/components/category/category.js
@@ -10,7 +10,7 @@ export default class category extends Component {
           <img src={this.props.img} alt={'Logo'} />
           </Col>
           <Col xs='8'>
-          <h5 style={{color: '#fff', fontWeight: 'bold'}}>{this.props.txt}</h5>
+          <h5 style={{color: '#fff', }}>{this.props.txt}</h5>
           </Col>
           <Col xs='2' style={{justifyContent: 'center', alignItems: 'center'}}>
           <img src={require('../../assets/images/arrow-down-white.png')} alt={'Logo'} style={{ height: 20, width: 20, alignSelf: 'center' }} />
--- src/demo1/index.js
+++ src/demo1/index.js
@@ -48,36 +48,64 @@ export class Demo1 extends Component {
         <Container fluid={true} >
           <Row >
             <Col xs='12' sm='4'  style={{padding: 1}}>
-            <Dropdown isOpen={this.state.dropdownOpen} toggle={this.toggle} style={{ margin: 0, backgroundColor: 'transparent', borderWidth: 0, borderColor: 'transparent', flex: 1, display: 'flex'}} onMouseOver={this.toggle}>
+            <Dropdown isOpen={this.state.dropdownOpen} toggle={this.toggle} style={{ margin: 0, backgroundColor: 'transparent', borderWidth: 0, borderColor: 'transparent', flex: 1, display: 'flex'}} onMouseEnter={this.toggle} onMouseLeave={this.toggle}>
                   <DropdownToggle style={{ backgroundColor: 'transparent', padding: 0, borderWidth: 0, borderColor: 'transparent', flex: 1, display: 'flex'}}>
                   <Category img={require('../assets/images/White Cart icon.png')} txt={'Marketplace'}/>
                   </DropdownToggle>
-                  <DropdownMenu>
-                    <DropdownItem >Automobile</DropdownItem>
-                    <DropdownItem>Baby Products</DropdownItem>
-                    <DropdownItem>Books, Movies and Music</DropdownItem>
-                    <DropdownItem>Computing</DropdownItem>
-                    <DropdownItem >Electronics</DropdownItem>
-                    <DropdownItem>Fashion</DropdownItem>
-                    <DropdownItem>Gaming</DropdownItem>
-                    <DropdownItem>Health & Beauty</DropdownItem>
-                    <DropdownItem >Home & Office</DropdownItem>
-                    <DropdownItem>Industrial & Scientific</DropdownItem>
-                    <DropdownItem>Musical Instruments</DropdownItem>
-                    <DropdownItem>Pet Supplies</DropdownItem>
-                    <DropdownItem>Phones & Tablets</DropdownItem>
-                    <DropdownItem >Services</DropdownItem>
-                    <DropdownItem>Sporting Goods</DropdownItem>
-                    <DropdownItem>Toys & Games</DropdownItem>
-                    <DropdownItem>Agricultural Commodities</DropdownItem>
-                    <DropdownItem>Generators and Power Solutions</DropdownItem>
+                  <DropdownMenu
+                    modifiers={{
+                      width: {
+                        enabled: true,
+                        order: 890,
+                        fn: (data) => {
+                          return {
+                            ...data,
+                            styles: {
+                              ...data.styles,
+                              overflow: 'auto',
+                              width: window.screen.width*0.6
+                            },
+                          };
+                        },
+                      }
+                    }}
+                  >
+                    <Container >
+                      <Row >
+                        <Col >
+                          <DropdownItem >Automobile</DropdownItem>
+                          <DropdownItem>Baby Products</DropdownItem>
+                          <DropdownItem>Books, Movies and Music</DropdownItem>
+                          <DropdownItem>Computing</DropdownItem>
+                          <DropdownItem >Electronics</DropdownItem>
+                          <DropdownItem>Fashion</DropdownItem>
+                        </Col>
+                        <Col >
+                          <DropdownItem>Gaming</DropdownItem>
+                          <DropdownItem>Health & Beauty</DropdownItem>
+                          <DropdownItem >Home & Office</DropdownItem>
+                          <DropdownItem>Industrial & Scientific</DropdownItem>
+                          <DropdownItem>Musical Instruments</DropdownItem>
+                          <DropdownItem>Pet Supplies</DropdownItem>
+                        </Col>
+                        <Col >
+                          <DropdownItem>Phones & Tablets</DropdownItem>
+                          <DropdownItem >Services</DropdownItem>
+                          <DropdownItem>Sporting Goods</DropdownItem>
+                          <DropdownItem>Toys & Games</DropdownItem>
+                          <DropdownItem>Agricultural Commodities</DropdownItem>
+                          <DropdownItem>Generators and Power Solutions</DropdownItem>
+                        </Col>
+                      </Row>
+                    </Container>
+
                   </DropdownMenu>
                 </Dropdown>
               
             </Col>
             <Col xs='12' sm='4'  style={{padding: 1}}>
 
-            <Dropdown isOpen={this.state.dropdownOpen2} toggle={this.toggle2} style={{backgroundColor: 'transparent', borderWidth: 0, borderColor: 'transparent', flex: 1, display: 'flex'}} onMouseOver={this.toggle2}>
+            <Dropdown isOpen={this.state.dropdownOpen2} toggle={this.toggle2} style={{backgroundColor: 'transparent', borderWidth: 0, borderColor: 'transparent', flex: 1, display: 'flex'}} onMouseEnter={this.toggle2} onMouseLeave={this.toggle2}>
                   <DropdownToggle style={{backgroundColor: 'transparent', borderWidth: 0, borderColor: 'transparent', flex: 1, display: 'flex', padding: 0}}>
                     <Category img={require('../assets/images/Knowledge Hub icon.png')} txt={'SME Knowledge Hub'}/> 
                   </DropdownToggle>
@@ -89,7 +117,7 @@ export class Demo1 extends Component {
                 </Dropdown>
             </Col>
             <Col xs='12' sm='4' style={{padding: 1}}>
-            <Dropdown isOpen={this.state.dropdownOpen3} toggle={this.toggle3} style={{backgroundColor: 'transparent', borderWidth: 0, borderColor: 'transparent', flex: 1, display: 'flex'}} onMouseOver={this.toggle3}>
+            <Dropdown isOpen={this.state.dropdownOpen3} toggle={this.toggle3} style={{backgroundColor: 'transparent', borderWidth: 0, borderColor: 'transparent', flex: 1, display: 'flex'}} onMouseEnter={this.toggle3} onMouseLeave={this.toggle3}>
                   <DropdownToggle style={{backgroundColor: 'transparent', borderWidth: 0, borderColor: 'transparent', flex: 1, display: 'flex', padding: 0}}>
                     <Category img={require('../assets/images/Biz Xchange Icon.png')} txt={'Business Exchange'}/>
                   </DropdownToggle>
